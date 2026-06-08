9a6b58b882865b656cca5d9a16df573da413a0cd
huynhyen255 <157729286+huynhyen255@users.noreply.github.com>
2026-06-09 06:43:55 +0700

    Fix indentation in load_trained_model function

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ st.write("---")
 # --- HÀM XỬ LÝ DỮ LIỆU & HUẤN LUYỆN SONG SONG ---
 @st.cache_resource
 def load_trained_model():
-df = pd.read_csv("2cls_spam_text_cls.csv", on_bad_lines='skip')
-ps = PorterStemmer()
+    df = pd.read_csv("2cls_spam_text_cls.csv", on_bad_lines='skip')
+    ps = PorterStemmer()
     
     def preprocess(text):
         text = str(text).lower().translate(str.maketrans("", "", string.punctuation))
